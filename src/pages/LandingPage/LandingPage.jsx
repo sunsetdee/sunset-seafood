7dcd57ad98542c927e890e9a8d585caed4198d22
sunsetdee <andiilii@yahoo.com>
2021-08-08 20:47:28 -0700

    landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './LandingPage.css';
 export default function LandingPage() {
 
   return (
     <>
         <main className="landing-page">
-            <h1>Landing page</h1>
+            <h1 className="landing-title">Sunset Seafood</h1>
         </main>
         <body>
-            <h1>Landing Page</h1>
+            <p className="landing-header">$$-Seafood-Meat-Vegan Friendly</p>
+            <p className="landing-header">Serving Global</p>
+            <h4 className="menu-all-day">Menu All Day</h4>
+            <p className="landing-hour">9:00AM - 9:00PM</p>
+            <nav>
+                <Link>Shrimps</Link>&nbsp;&nbsp;
+                <Link>Crab</Link>&nbsp;&nbsp;
+                <Link>Fish</Link>&nbsp;&nbsp;
+                <Link>Shell Fish</Link>&nbsp;&nbsp;
+                <Link>Exoctic Animal</Link>&nbsp;&nbsp;
+                <Link>Beef</Link>&nbsp;&nbsp;
+                <Link>Vegetable</Link>&nbsp;&nbsp;
+            </nav>
+
         </body>
     </>
     
