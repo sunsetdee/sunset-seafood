e1050af72fdc778c916a23fb1915f96f0764caa7
sunsetdee <andiilii@yahoo.com>
2021-08-11 14:35:08 -0700

    plant seed

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -17,13 +17,14 @@ export default function LandingPage() {
             <h4 className="menu-all-day">Menu All Day</h4>
             <p className="landing-hour">9:00AM - 9:00PM</p>
             <nav className="landing-category">
+                <Link>Appetizers and Snacks</Link>
                 <Link>Shrimps</Link>&nbsp;&nbsp;
-                <Link>Crab</Link>&nbsp;&nbsp;
+                <Link>Crab and Lobster</Link>&nbsp;&nbsp;
                 <Link>Fish</Link>&nbsp;&nbsp;
                 <Link>Shell Fish</Link>&nbsp;&nbsp;
                 <Link>Snake</Link>&nbsp;&nbsp;
                 <Link>Insects</Link>&nbsp;&nbsp;
-                <Link>Exoctic Animal</Link>&nbsp;&nbsp;
+                <Link>Hot Pot</Link>&nbsp;&nbsp;
                 <Link>Beef</Link>&nbsp;&nbsp;
                 <Link>Pork and Chicken</Link>&nbsp;&nbsp;
                 <Link>Vegetable</Link>&nbsp;&nbsp;
